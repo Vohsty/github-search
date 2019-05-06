927fe08a3f33c5758ebe74d83067e8cedb3f2505
Vohsty <stevekimanthi87@gmail.com>
2019-05-06 18:09:48 +0300

    add repoholder constructor

--- src/app/repo-holder.ts
+++ src/app/repo-holder.ts
@@ -1,5 +1,5 @@
-// export class RepoHolder {
-//     constructor( public name:string, public description:string, public language:string, public url:string) {
-//     }
+export class RepoHolder {
+    constructor( public name:string, public description:string, public language:string, public url:string) {
+    }
   
-// }
+}
