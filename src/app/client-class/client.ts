bc4ae40b12c59e1e8d14dc529a461f746184fe94
Vohsty <stevekimanthi87@gmail.com>
2019-05-06 17:56:00 +0300

    add client.ts constructor

--- src/app/client-class/client.ts
+++ src/app/client-class/client.ts
@@ -1,15 +1,15 @@
-// export class Client {
-//     constructor( 
-//         public avatarimg:string, 
-//         public user:string, 
-//         public owner:string,
-//         public email:string, 
-//         public bio:string,
-//         public followers: number, 
-//         public following: number, 
-//         public noOfrepos: number, 
-//         public gistNo: number, 
-//         public date: any) {
+export class Client {
+    constructor( 
+        public avatarimg:string, 
+        public user:string, 
+        public owner:string,
+        public email:string, 
+        public bio:string,
+        public followers: number, 
+        public following: number, 
+        public noOfrepos: number, 
+        public gistNo: number, 
+        public date: any) {
         
-//     } 
-// }
+    } 
+}
